ea625dfd4185bab7f6024e4821c5dc6c3db19224
Wilfer <whoquendo@hotmail.com>
2024-01-16 10:47:08 +0100

    fix merge with develop branch

--- src/main/java/be/wilferoquendo/Enregistrement_de_heures_de_travail/bl/exception/UserNotFoundException.java
+++ src/main/java/be/wilferoquendo/Enregistrement_de_heures_de_travail/bl/exception/UserNotFoundException.java
@@ -1,7 +1,6 @@
 package be.wilferoquendo.Enregistrement_de_heures_de_travail.bl.exception;
 
 public class UserNotFoundException extends RuntimeException {
-
     public UserNotFoundException (String message) {
     super(message);
     }
